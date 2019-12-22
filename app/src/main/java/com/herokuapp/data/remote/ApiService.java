6f75b5b422ddbcdd0149256e5a29a11f9b9996c0
Abhijeet Gupta <abhijeet.gupta@pb.com>
2019-12-22 23:33:09 +0530

    Update retrofit query param name

--- app/src/main/java/com/herokuapp/data/remote/ApiService.java
+++ app/src/main/java/com/herokuapp/data/remote/ApiService.java
@@ -10,12 +10,12 @@ import retrofit2.http.Query;
 
 public interface ApiService {
 
-    @GET("authors?_limit=" + ApiConstants.USER_FETCH_LIMIT + "&_page=1")
+    @GET("authors?_limit=" + ApiConstants.USER_FETCH_LIMIT + "&_page={pageNo}")
     Call<AuthorsResponse> fetchAuthor(@Query("_page") int pageNo);
 
     @GET("posts?authorId={authorId}&_sort=date&_order=desc&_page={pageNo}&_limit=" + ApiConstants.POSTS_FETCH_LIMIT)
-    Call<PostsResponse> fetchPosts(@Path("authorId") String authorId,@Path("_page") int pageNo);
+    Call<PostsResponse> fetchPosts(@Query("authorId") String authorId,@Query("_page") int pageNo);
 
     @GET("posts?postId={authorId}&_sort=date&_order=desc&_page=1&_limit=" + ApiConstants.POSTS_FETCH_LIMIT)
-    Call<PostsResponse> fetchComments(@Path("postId") String postId, @Path("_page") int pageNo);
+    Call<PostsResponse> fetchComments(@Query("postId") String postId, @Query("_page") int pageNo);
 }
